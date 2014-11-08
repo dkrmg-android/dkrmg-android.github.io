3e37a5efea8576438cc6043b9cfa1e5441a30e99
Gyuri <gdenes355@gmail.com>
2014-11-08 01:49:47 +0000

    Slight changes in Style
    + big FAQ commit

---
+++ include/gyik/lifeCycle.java
@@ -0,0 +1,23 @@
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_ACTIVITYNEVE);
+
+}
+
+@Override
+protected void onResume() {
+  super.onResume();
+
+}
+
+@Override
+protected void onPause() {
+  super.onPause();
+
+}
+
+@Override
+protected void onDestroy() {
+  super.onDestroy();
+}
---
+++ include/gyik/onClick.java
@@ -0,0 +1,4 @@
+public void buttonOnClick(View v)
+{
+
+}  
